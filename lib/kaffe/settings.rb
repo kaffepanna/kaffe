4314c2e579e49aed9029b06b9bcc265a53831791
Patrik Pettersson <pettersson.pa@gmail.com>
2010-06-18 14:34:47 +0200

    inherited settings

--- lib/kaffe/settings.rb
+++ lib/kaffe/settings.rb
@@ -5,14 +5,24 @@ module Kaffe
       def set option, value
         case value
         when Proc
-          define_singleton_method(option, value)
+          settings.define_singleton_method(option, value)
         else
           set(option, proc { value })
         end
       end
+
+      def settings
+        @settings ||= if superclass.respond_to? :settings
+          Class.new(superclass.settings)
+        else
+          Class.new
+        end
+      end
     end
-    
-    def settings; self.class; end
+
+    def settings
+      self.class.settings
+    end
 
     def self.included(base)
       base.extend(ClassMethods)
