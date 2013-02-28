bf6af9ebfd8ae65a6ccd36e7832807645c10f17c
Patrik Pettersson <pettersson.pa@gmail.com>
2013-02-28 22:48:55 +0100

    Simple layout template

--- lib/kaffe/render.rb
+++ lib/kaffe/render.rb
@@ -5,10 +5,37 @@ module Kaffe
       @templates ||= Tilt::Cache.new
     end
 
-    def render(template, options = {:layout => true} )
-      t = templates.fetch(template) { Tilt.new template }
+    def render(template, options: {}, &block)
+      local = options.delete(:locals) || {}
+      views = options.delete(:views) || settings.views || "./views"
+      layout = options[:layout] || settings.layout || :layout
 
-      return t.render(self)
+      case template
+      when Symbol
+        path = nil
+        found = false
+        find_template(views, template, engine) do |file|
+          puts "testing #{file}"
+          if found = File.exists?(file)
+            path = file
+            break
+          end
+        end
+        raise "Could not find Template #{template}" unless found
+        t = templates.fetch(path) { Tilt.new path }
+        output = t.render(self, &block)
+
+        if layout
+          return render(engine, layout, layout: false) { output }
+        end
+        return output
+      end
+    end
+
+    def find_template(views, name, engine)
+      Tilt.mappings.each do |ext, engines|
+        yield ::File.join(views, "#{name}.#{ext}")
+      end
     end
   end
 end
